bab7789aabf202a94b35301f4bf76baf9a655478
Tirth Kesharia <tirth.kesharia@gmail.com>
2026-02-21 14:48:36 +0530

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,3 +5,14 @@ self.addEventListener("install", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(fetch(event.request));
 });
+
+const urlsToCache = [
+  "index.html",
+  "style.css",
+  "app.js",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png",
+  "apple-touch-icon.png",
+  "favicon.png"
+];
